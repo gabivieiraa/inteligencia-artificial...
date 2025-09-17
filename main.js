da80419eaed550516bf504a27ea30769eee454a6
gabivieiraa <159184847+gabivieiraa@users.noreply.github.com>
2025-09-17 11:25:36 -0300

    17-09...

--- main.js
+++ main.js
@@ -75,12 +75,17 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
-
+let historiaFinal = " ";
 
 function mostraPergunta(){
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -88,22 +93,23 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa.texto;
-    botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
    }
 
 }
-function respostaSelecionada(opcaoSelecionada) {
+function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes +"" ;
     atual++;
     mostraPergunta();
 
 }
-function mostraResultados() {
-    caixaPerguntas.textContent = "em 2049";
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+
 }
 
 mostraPergunta();
